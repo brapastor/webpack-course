67a57b348b6d1cb76a20fc6ec3fe309326751676
brapastor <brapastor@gmail.com>
2018-05-15 20:04:42 -0500

    agregado de ddl

---
+++ prevent-duplication/src/js/index.js
@@ -0,0 +1,26 @@
+import '../css/estilos.css'
+import {firstMessage, delayMessage} from './message.js'
+import platziImg from '../img/platzi.png'
+import data from './teachers'
+import renderToDOM from './render-to-dom'
+import React from 'react'
+import {render} from 'react-dom'
+import Teachers from './components/teachers'
+
+render(<Teachers data={data}/>, document.getElementById('container'))
+console.log(data);
+
+data.teachers.forEach((teacher) => {
+    const element = document.createElement('li');
+    element.textContent = teacher.name;
+    renderToDOM(element);
+})
+
+const img = document.createElement('img')
+img.setAttribute('src', platziImg);
+img.setAttribute('width', 50);
+img.setAttribute('height', 50);
+document.body.append(img);
+document.write(firstMessage);
+delayMessage()
+console.log('Hola mundo, desde webpack external')
