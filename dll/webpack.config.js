d9b702c4dd34c9def579c5c442a4a5a91c9d0ef7
brapastor <brapastor@gmail.com>
2018-05-23 19:34:16 -0500

    agregado de dll

--- dll/webpack.config.js
+++ dll/webpack.config.js
@@ -3,10 +3,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const webpack = require('webpack')
 module.exports = {
     entry: {
-        vendor: [
-           'react',
-            'react-dom'
-        ],
+        // vendor: [
+        //     'react',
+        //     'react-dom'
+        // ],
         home: path.resolve(__dirname, './src/js/index.js'),
         contact: path.resolve(__dirname, './src/js/contact.js'),
     },
@@ -14,59 +14,59 @@ module.exports = {
         path: path.resolve(__dirname, './dist/'),
         filename: '[name].js'
     },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.js$/,
-    //             use: {
-    //                 loader: "babel-loader",
-    //                 options: {
-    //                     presets: ['es2015', 'react']
-    //                 }
-    //             },
-    //         },
-    //         {
-    //             test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
-    //             use: {
-    //                 loader: 'url-loader',
-    //                 options: {
-    //                     limit: 100000
-    //                 }
-    //             }
-    //         },
-    //         //    Aqui van los loaders
-    //         {
-    //             //test: que tipo de archivo quiero reconocer,
-    //             //use: que loader se va a encargar del archivo
-    //             test: /\.css$/,
-    //             use: ExtractTextPlugin.extract({
-    //                 // fallback: 'style-loader',
-    //                 use: [
-    //                     {
-    //                         loader: 'css-loader',
-    //                         options: {
-    //                             modules: true,
-    //                             importLoaders: 1
-    //                         }
-    //                     },
-    //                     'postcss-loader'
-    //                 ]
-    //             })
-    //         },
-    //         {
-    //             test: /\.scss$/,
-    //             use: ExtractTextPlugin.extract({
-    //                 // fallback: 'style-loader',
-    //                 use: ["css-loader", "sass-loader"]
-    //             })
-    //         }
-    //     ]
-    // },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                },
+            },
+            {
+                test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 100000
+                    }
+                }
+            },
+            //    Aqui van los loaders
+            {
+                //test: que tipo de archivo quiero reconocer,
+                //use: que loader se va a encargar del archivo
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    // fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                importLoaders: 1
+                            }
+                        },
+                        'postcss-loader'
+                    ]
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    // fallback: 'style-loader',
+                    use: ["css-loader", "sass-loader"]
+                })
+            }
+        ]
+    },
     plugins: [
         //Aqui van los plugins
-        // new ExtractTextPlugin("css/[name].css")
-        new webpack.DllPlugin({
-            path:
+        new ExtractTextPlugin("css/[name].css"),
+        new webpack.DllReferencePlugin({
+            manifest: require('./modules-manifest.json')
         })
     ],
     // optimization: {
--- dll/webpack.dll.config.js
+++ dll/webpack.dll.config.js
@@ -1,76 +1,23 @@
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
+const webpack = require('webpack')
 module.exports = {
     entry: {
-        vendor: [
-           'react',
+        modules: [
+            'react',
             'react-dom'
         ],
-        home: path.resolve(__dirname, './src/js/index.js'),
-        contact: path.resolve(__dirname, './src/js/contact.js'),
     },
     output: {
         path: path.resolve(__dirname, './dist/'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['es2015', 'react']
-                    }
-                },
-            },
-            {
-                test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 100000
-                    }
-                }
-            },
-            //    Aqui van los loaders
-            {
-                //test: que tipo de archivo quiero reconocer,
-                //use: que loader se va a encargar del archivo
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    // fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true,
-                                importLoaders: 1
-                            }
-                        },
-                        'postcss-loader'
-                    ]
-                })
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    // fallback: 'style-loader',
-                    use: ["css-loader", "sass-loader"]
-                })
-            }
-        ]
+        filename: '[name].js',
+        library: "[name]"
     },
     plugins: [
         //Aqui van los plugins
-        new ExtractTextPlugin("css/[name].css")
-    ],
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            minChunks: Infinity,
-            chunks: 'initial'
-        }
-    }
+        new webpack.DllPlugin({
+            name: "[name]",
+            path: path.join(__dirname, "[name]-manifest.json")
+        })
+    ]
 }
